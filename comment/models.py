48d31407828902d8f062fbd1151280959a4e924e
Korsunow Konstantin <korsunowk@yandex.ua>
2017-04-09 22:53:48 +0300

    readable name for comment in admin panel

--- comment/models.py
+++ comment/models.py
@@ -8,3 +8,6 @@ class Comment(MPTTModel):
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
     text = models.TextField('Comment', max_length=2*1024)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.user.username, self.text)
