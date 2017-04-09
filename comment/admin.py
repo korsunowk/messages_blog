e01822cb260befee2219aad9a04c6bb9cc529f68
Korsunow Konstantin <korsunowk@yandex.ua>
2017-04-09 22:55:51 +0300

    add display username of user in admin panel

--- comment/admin.py
+++ comment/admin.py
@@ -3,7 +3,11 @@ from comment.models import Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['text', 'created']
+    list_display = ['get_creator_name', 'text', 'created']
 
+    def get_creator_name(self, obj):
+        return obj.user.username
+
+    get_creator_name.short_description = 'Username'
 
 admin.site.register(Comment, CommentAdmin)
