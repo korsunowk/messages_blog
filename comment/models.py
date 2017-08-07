3e7eab009232bf80161b42a8e810c8b57cb8bd9d
Korsunow Konstantin <korsunowk@gmail.com>
2017-08-07 07:48:10 +0300

    Update models.py

--- comment/models.py
+++ comment/models.py
@@ -1,6 +1,7 @@
-from mptt.models import MPTTModel, TreeForeignKey
 from django.db import models
 from django.contrib.auth.models import User
+
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Comment(MPTTModel):
